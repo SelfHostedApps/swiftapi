bcb5c2ed526e2e727beca91e50224cd8cf1c293d
Lililatortue <will.descoteaux@gmail.com>
2025-10-27 15:27:49 -0400

    modify jenkins

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 set -e
 
-PODMAN=podman
-
 echo ">> creating pod"
 #create pod
-$PODMAN pod create --replace \
+podman pod create --replace \
   --name swiftapiapp \
   -p 5011:5011
   
 echo "Creating Images"
 echo "--------------------------------"
 echo ">> creating swiftapi image"
-$PODMAN build -t dotnet-app-image -f ./images/dotnet-image .
+podman build -t dotnet-app-image -f ./images/dotnet-image .
 
 echo "Creating Containers"
 echo "--------------------------------"
 echo ">> creating swiftapi container"
-$PODMAN run --replace -d \
+podman run --replace -d \
   --name swiftapi \
   --pod swiftapiapp \
   dotnet-app-image  
 
 echo ">> creating postgres container"
-$PODMAN run --replace -d \
+podman run --replace -d \
   --name postgres \
   --pod swiftapiapp \
   -e POSTGRES_USER=swiftuser \
