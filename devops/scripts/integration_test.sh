e77909c383b864f516e23d91b8e28e1bb9439d2b
Lililatortue <will.descoteaux@gmail.com>
2025-11-03 15:25:50 -0500

    Corrected Jenkinsfile

--- devops/scripts/integration_test.sh
+++ devops/scripts/integration_test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 # go to .git level of project
-cd "$(git rev-parse --show-toplevel 2>dev>null || echo "$(dirname "$0")/../..")"
-
+cd "$(git rev-parse --show-toplevel 2>/dev/null || echo "$(dirname "$0")/../..")"
 
 echo "testing users endpoint"
-token=$(bash "$SCRIPT_DIR/integration_test/user.sh")
+token=$(bash "./devops/scripts/integration_test/user.sh")
         
-
 echo "testing tasks endpoint"
-bash "$SCRIPT_DIR/integration_test/task.sh" $"token"
+bash "./devops/scripts/integration_test/task.sh" "$token"
