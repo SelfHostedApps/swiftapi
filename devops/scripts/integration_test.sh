8ba3d2e5df0b28249f84671b8394189328585f2c
Lililatortue <will.descoteaux@gmail.com>
2025-11-03 14:13:05 -0500

    Corrected Jenkinsfile

--- devops/scripts/integration_test.sh
+++ devops/scripts/integration_test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 echo "testing users endpoint"
-token=(bash ./integration_test/user.sh)
+token=$(bash "$SCRIPT_DIR/integration_test/user.sh")
         
 
 echo "testing tasks endpoint"
-bash ./integration_test/task.sh $"token"
+bash "$SCRIPT_DIR/integration_test/task.sh" $"token"
