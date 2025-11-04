7241c7dedabb282e3fc10999def2d7dd2d4a4825
Lililatortue <will.descoteaux@gmail.com>
2025-11-03 19:42:22 -0500

    jenkins

--- devops/scripts/integration_test.sh
+++ devops/scripts/integration_test.sh
@@ -5,8 +5,8 @@ cd "$(git rev-parse --show-toplevel 2>/dev/null || echo "$(dirname "$0")/../..")
 
 
 echo "testing users endpoint"
-token=$(bash "./devops/scripts/integration_test/user.sh")
+token=$(bash "/integration_test/user.sh")
 
 
 echo "testing tasks endpoint"
-bash "./devops/scripts/integration_test/task.sh" "$token"
+bash "/integration_test/task.sh" "$token"
