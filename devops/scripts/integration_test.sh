1fde6d488ad4d5b12b2b4237c8c4ed0ba67d1de7
Lililatortue <will.descoteaux@gmail.com>
2025-11-03 21:13:23 -0500

    jenkins

--- devops/scripts/integration_test.sh
+++ devops/scripts/integration_test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
+
+
 # go to .git level of project
 cd "$(git rev-parse --show-toplevel 2>/dev/null || echo "$(dirname "$0")/../..")"
 
 
 echo "testing users endpoint"
-token=$(bash "/integration_test/user.sh")
+token=$(bash "devops/scripts/integration_test/user.sh")
 
 
 echo "testing tasks endpoint"
-bash "/integration_test/task.sh" "$token"
+bash "devops/scripts/integration_test/task.sh" "$token"
